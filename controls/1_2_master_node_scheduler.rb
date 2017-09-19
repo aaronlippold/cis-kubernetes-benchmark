1bfa2e62091dd87244eb09e4a47d022073a9bd2d
HackerShark <melsharkawi@mitre.org>
2017-09-19 13:12:51 -0400

    completed control information for the 1.2 section

--- controls/1_2_master_node_scheduler.rb
+++ controls/1_2_master_node_scheduler.rb
@@ -22,12 +22,34 @@ only_if do
 end
 
 control 'cis-kubernetes-benchmark-1.2.1' do
-  title 'Ensure that the --profiling argument is set to false'
-  desc "Disable profiling, if not needed.\n\nRationale: Profiling allows for the identification of specific performance bottlenecks. It generates a significant amount of program data that could potentially be exploited to uncover system and program details. If you are not experiencing any bottlenecks and do not need the profiler for troubleshooting purposes, it is recommended to turn it off to reduce the potential attack surface."
+  title 'Ensure that the --profiling argument is set to false (Scored)'
+  desc "Disable profiling, if not needed."
   impact 1.0
 
-  tag cis: 'kubernetes:1.2.1'
-  tag level: 1
+  tag rationale: "Profiling allows for the identification of specific performance bottlenecks. It generates a significant amount of program data that could potentially be exploited to uncover system and program details. If you are not experiencing any bottlenecks and do not need the profiler for troubleshooting purposes, it is recommended to turn it off to reduce the potential attack surface."
+
+  tag check: "Run the following command on the master node:
+
+  `ps -ef | grep kube-scheduler`
+
+  Verify that the `--profiling` argument is set to `false`."
+
+  tag fix: "Edit the `/etc/kubernetes/scheduler` file on the master node and set the `KUBE_SCHEDULER_ARGS` parameter to `"--profiling=false"`:
+
+  `KUBE_SCHEDULER_ARGS="--profiling=false"`
+
+  Based on your system, restart the `kube-scheduler` service. For example:
+
+  `systemctl restart kube-scheduler.service`"
+
+  tag cis_family: ['14', '6.1']
+  tag cis_rid: "1.2.1"
+  tag cis_level: 1
+  tag nist: ['AC-6', '4']
+
+  ref 'kube-scheduler', url: 'https://kubernetes.io/docs/admin/kube-scheduler/'
+  ref 'profiling.md', url: 'https://github.com/kubernetes/community/blob/master/contributors/devel/profi
+ling.md'
 
   describe processes('kube-scheduler').commands.to_s do
     it { should match(/--profiling=false/) }
