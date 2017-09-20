322af9649c2eb3e0a5d0e2e8c032db1c442e611b
HackerShark <melsharkawi@mitre.org>
2017-09-20 10:57:44 -0400

    completed control information for the 3.2 section

--- controls/3_2_federation_controller_manager.rb
+++ controls/3_2_federation_controller_manager.rb
@@ -22,12 +22,30 @@ only_if do
 end
 
 control 'cis-kubernetes-benchmark-3.2.1' do
-  title 'Ensure that the --profiling argument is set to false'
-  desc "Disable profiling, if not needed.\n\nRationale: Profiling allows for the identification of specific performance bottlenecks. It generates a significant amount of program data that could potentially be exploited to uncover system and program details. If you are not experiencing any bottlenecks and do not need the profiler for troubleshooting purposes, it is recommended to turn it off to reduce the potential attack surface."
+  title 'Ensure that the --profiling argument is set to false (Scored)'
+  desc "Disable profiling, if not needed."
   impact 1.0
 
-  tag cis: 'kubernetes:3.2.1'
-  tag level: 1
+  tag rationale: "Profiling allows for the identification of specific performance bottlenecks. It generates a significant amount of program data that could potentially be exploited to uncover system and program details. If you are not experiencing any bottlenecks and do not need the profiler for troubleshooting purposes, it is recommended to turn it off to reduce the potential attack surface."
+
+  tag check: "Run the following command:
+
+  `ps -ef | grep federation-controller-manager`
+
+  Verify that the `--profiling` argument is set to `false`."
+
+  tag fix: "Edit the deployment specs and set `\"--profiling=false\"`:
+
+  `kubectl edit deployments federation-controller-manager-deployment --namespace=federation-system`"
+
+  tag cis_family: ['14', '6.1']
+  tag cis_rid: "3.2.1"
+  tag cis_level: 1
+  tag nist: ['AC-6', '4']
+
+  ref 'federation-controller-manager', url: 'https://kubernetes.io/docs/admin/federation-controller-manager/'
+  ref 'profiling', url: 'https://github.com/kubernetes/community/blob/master/contributors/devel/profiling.md'
+  ref 'federation-controller-manager-deployment', url: 'https://github.com/kubernetes/kubernetes/blob/master/federation/manifests/federation-controller-manager-deployment.yaml'
 
   describe processes('federation-controller-manager').commands.to_s do
     it { should match(/--profiling=false/) }
